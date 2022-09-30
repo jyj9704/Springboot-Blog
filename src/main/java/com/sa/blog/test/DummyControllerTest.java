f4306d93618d9d81f8bbdc29af3529bc9435350e
YEJI JO <96167713+jyj9704@users.noreply.github.com>
2022-10-01 00:23:47 +0900

    09/30

--- src/main/java/com/sa/blog/test/DummyControllerTest.java
+++ src/main/java/com/sa/blog/test/DummyControllerTest.java
@@ -1,11 +1,18 @@
 package com.sa.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sa.blog.model.RoleType;
@@ -18,6 +25,33 @@ public class DummyControllerTest {
 	
 	@Autowired // 같이 메모리에 뜨도록 // 의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	// email, password
+	// RequestBody -> 제이슨 데이터 받기 위해 작성
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		System.out.println("id : "+id);
+		System.out.println("password : "+requestUser.getPassword());
+		System.out.println("email : "+requestUser.getEmail());
+		
+		return null;
+	}
+	
+	// http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list() {
+		return userRepository.findAll();
+	}
+	
+	//한 페이지당 2건에 데이터를 리턴받아
+	@GetMapping("/dummy/user")
+	public List <User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC)Pageable pageable) {
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+	
 	
 	@GetMapping("/dummy/user/{id}")
 	public User detail(@PathVariable int id) {
