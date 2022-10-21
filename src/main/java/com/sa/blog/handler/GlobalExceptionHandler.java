3f40844c501b89836077c486a1518a93b4b8c03d
YEJI JO <96167713+jyj9704@users.noreply.github.com>
2022-10-21 22:36:52 +0900

    10/21
    에러 해결 안됌..

--- src/main/java/com/sa/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sa/blog/handler/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.sa.blog.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sa.blog.dto.ResponseDto;
 
 @ControllerAdvice
 @RestController
@@ -10,7 +13,7 @@ public class GlobalExceptionHandler {
 
 	//해당 사용자가 없습니다.
 	@ExceptionHandler(value=Exception.class)
-	public String handleArgumentException(IllegalArgumentException e) {
-		return "<h1>"+e.getMessage()+"</h1>";
+	public ResponseDto<String> handleArgumentException(Exception e) {
+		return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
 	}
 }
